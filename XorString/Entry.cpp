7f9cefab39b58c0426769716d9e7f2f960adc214
pepsi <82460029+PePsIDeveloper@users.noreply.github.com>
2022-12-17 18:47:58 -0700

    Delete XorString directory

--- XorString/Entry.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils/XorUtils.hpp"
-int main() {
-	XorString xor_string("Hello, world!", "1234567890qwertyuiopasdfghjklzxcvbnm");
-	std::string encrypted = xor_string.Encrypt("pepsi_was_never_here");
-	std::cout << "Encrypted: " << encrypted << std::endl;
-	std::cout << "Decrypted: " << xor_string.Decrypt("pepsi_was_never_here") << std::endl;
-	return 0;
-}
