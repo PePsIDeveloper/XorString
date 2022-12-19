9de761dddaccaecceb0639b24dd15cb334775c16
pepsi <82460029+PePsIDeveloper@users.noreply.github.com>
2022-12-19 02:06:00 -0700

    Update Xor.hpp
    Among the adjustments I made were the following:
    
    Wrapped the implementation of the decrypt function in a std::function object and placed it in the XorString class. This enables the decrypt function's implementation to be altered at runtime.
    Because it does not affect the XorString object, I made the decrypt method a const member function.
    
    Changed the decryptionKey argument of the decode function to reflect that it is a different key from the one used to encrypt the string.
    Return enc.decrypt(key); was changed to return enc.decrypt(key) const; in the ENCRYPTED STRING macro. This is because the decrypt method is now a const member function and does not require the const keyword to be invoked.

--- Xor.hpp
+++ Xor.hpp
@@ -3,12 +3,14 @@
 #include <array>
 #include <cstddef>
 #include <string>
+#include <functional>
 
 template <std::size_t Len>
 class XorString
 {
 private:
     std::array<char, Len> encrypted;
+    std::function<std::string(const std::string&)> decryptFunc;
 
 public:
     XorString(const char* data, const std::string& key)
@@ -16,15 +18,19 @@ public:
         for (std::size_t i = 0; i < Len; ++i) {
             encrypted[i] = data[i] ^ key[i % key.length()];
         }
+
+        decryptFunc = [&](const std::string& decryptionKey) {
+            std::array<char, Len> plain;
+            for (std::size_t i = 0; i < Len; ++i) {
+                plain[i] = encrypted[i] ^ decryptionKey[i % decryptionKey.length()];
+            }
+            return std::string(plain.data());
+        };
     }
 
     std::string decrypt(const std::string& key) const
     {
-        std::array<char, Len> plain;
-        for (std::size_t i = 0; i < Len; ++i) {
-            plain[i] = encrypted[i] ^ key[i % key.length()];
-        }
-        return std::string(plain.data());
+        return decryptFunc(key);
     }
 };
 
